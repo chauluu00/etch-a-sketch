59891c0936548078ce717352010c093e40317c8c
Chau Luu <68700957+chauluu00@users.noreply.github.com>
2024-09-02 13:39:56 -0700

    Add a prompt to take user input

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 const container = document.querySelector("#container");
-//Build grid
-for (let i = 0; i < 9; i++){
-    const input = 3;
-    const div = document.createElement("div");
-    div.style.width = container.clientWidth / input + "px";
-    div.style.height = container.clientHeight / input + "px";
-    container.appendChild(div);
+let input = 16
+buildGrid();
+
+//Build grid, color in and reset
+function buildGrid(){
+    for (let i = 0; i < input**2; i++){
+        const div = document.createElement("div");
+        div.style.width = container.clientWidth / input + "px";
+        div.style.height = container.clientHeight / input + "px";
+        container.appendChild(div);
+    }
+
+    //Mouseover sketchpad to color
+    const squares = container.querySelectorAll("div");
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", ()=> {
+            square.style.backgroundColor = "black";
+        });
+    });
+     
+    //Click button to erase sketchpad
+    const resetButton = document.querySelector("#reset");
+    resetButton.addEventListener("click", () => {
+        squares.forEach((square) => {
+            square.style.backgroundColor = "";
+        });
+    });
 }
 
-//Mouseover to color
-const squares = container.querySelectorAll("div");
-squares.forEach((square) => {
-    square.addEventListener("mouseover", ()=> {
-        square.style.backgroundColor = "black";
-    });
-});
+//Click button to resize sketchpad
+const resizeButton = document.querySelector("#resize");
+resizeButton.addEventListener("click", getUserInput);
 
-//Reset button
-const resetButton = document.querySelector("#reset");
-resetButton.addEventListener("click", () => {
-    squares.forEach((square) => {
-        square.style.backgroundColor = "";
-    });
-});
+//Popup asking for the number of squares per side
+function getUserInput() {
+    input = prompt("Please enter the number of squares");
+    if (input > 0 && input <= 100){
+        container.textContent = "";
+        buildGrid();
+    } else {
+        alert("Your input is invalid. Please enter a number between 1 and 100.");
+    }
+};
