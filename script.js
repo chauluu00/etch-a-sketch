0b4383f5434ccff489bf1dba86e7cdcbf6e2a0ef
Chau Luu <68700957+chauluu00@users.noreply.github.com>
2024-09-02 15:18:46 -0700

    Add menu with ink options

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-const container = document.querySelector("#container");
+const sketchpad = document.querySelector("#sketchpad");
+const gridSize = document.querySelector("p");
 let input = 16
+//Display grid size
+gridSize.textContent = `Current grid size: ${input} x ${input}`;
 buildGrid();
 
 //Build grid, color in and reset
 function buildGrid(){
     for (let i = 0; i < input**2; i++){
         const div = document.createElement("div");
-        div.style.width = container.clientWidth / input + "px";
-        div.style.height = container.clientHeight / input + "px";
-        container.appendChild(div);
+        div.style.width = sketchpad.clientWidth / input + "px";
+        div.style.height = sketchpad.clientHeight / input + "px";
+        sketchpad.appendChild(div);
     }
 
     //Mouseover sketchpad to color
-    const squares = container.querySelectorAll("div");
+    const squares = sketchpad.querySelectorAll("div");
     squares.forEach((square) => {
         square.addEventListener("mouseover", ()=> {
             square.style.backgroundColor = "black";
@@ -36,9 +39,49 @@ resizeButton.addEventListener("click", getUserInput);
 function getUserInput() {
     input = prompt("Please enter the number of squares");
     if (input > 0 && input <= 100){
-        container.textContent = "";
+        sketchpad.textContent = "";
         buildGrid();
     } else {
         alert("Your input is invalid. Please enter a number between 1 and 100.");
     }
+    gridSize.textContent = `Current grid size: ${input} x ${input}`;
 };
+
+//Add different ink types
+const menu = document.querySelector("#menu");
+menu.addEventListener("click", (event) => {
+    let target = event.target;
+    const squares = sketchpad.querySelectorAll("div");
+    switch(target.id){
+        case "black":
+            squares.forEach((square) => {
+                square.addEventListener("mouseover", ()=> {
+                    square.style.backgroundColor = "black";
+                });
+            });
+            break;
+        case "rainbow":
+            squares.forEach((square) => {
+                square.addEventListener("mouseover", ()=> {
+                    //Randomize rgb
+                    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+                    const r = randomBetween(0, 255);
+                    const g = randomBetween(0, 255);
+                    const b = randomBetween(0, 255);
+                    const rgb = `rgb(${r},${g},${b})`;
+                    square.style.backgroundColor = rgb;
+                });
+            });
+            break;
+        case "gradual":  
+            squares.forEach((square) => {
+                let l = 100;
+                square.addEventListener("mouseover", ()=> {
+                    l -= 10;
+                    const hsl = `hsl(0,0%,${l}%)`;
+                    square.style.backgroundColor = hsl;
+                });
+            });
+            break;
+    }
+});
